e7d4b114c836c946adfe5c809e97e2731f587a52
jovjou <jovjoudev@gmail.com>
2024-04-07 16:00:44 -0400

    add presentation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,19 +2,26 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Hello Tippio ⚡</title>
+</svelte:head>
+
 <div class="flex flex-col min-h-screen bg-black text-gray-50 bg-[url('/bg.webp')] bg-cover">
 	<main class="grow max-w-screen-xl mx-auto px-6 w-full flex flex-col items-center">
-		<h1 class="text-5xl center font-bold font-poppins my-16 md:my-24">Tippio</h1>
+		<h1 class="center my-16 md:mt-24 md:mb-12">
+			<img src="/tippio.png" alt="Tippio" class="w-[310px] h-[80px]" />
+		</h1>
 		<h2 class="text-4xl md:text-6xl center font-bold font-open-sans text-gray-200 text-center">
 			Revolutionizing Tipping
 		</h2>
-		<h3 class="text-xl center mt-5 mb-6 max-w-lg text-center text-[#D3D3D3] font-open-sans">
+		<h3 class="text-[18px] center mt-5 mb-6 max-w-lg text-center text-[#D3D3D3] font-open-sans">
 			Tippio, the innovative web application that empowers seamless tipping within the Solana
 			ecosystem
 		</h3>
 		<a
-			href="#"
-			class="bg-[#4800FF] hover:bg-[#4840FF] text-gray-200 font-poppins font-bold py-4 px-10 rounded"
+			href="/Tippio.pdf"
+			target="_blank"
+			class="text-[14px] bg-[#4800FF] hover:bg-[#4840FF] text-gray-200 font-poppins font-bold py-4 px-10 rounded"
 			>Get Started</a
 		>
 		<img src="/browser.webp" alt="Browser" class="mt-16 mb-16 md:mt-32 md:mb-24 mx-auto block" />
